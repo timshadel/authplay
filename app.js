c530651ba1e95f0ada712a381d2eecbb2fcbb9f9
Tim Shadel <tim@shadelsoftware.com>
2013-06-19 10:18:19 -0600

    Start adding app-provided page rendering

--- app.js
+++ app.js
@@ -3,4 +3,13 @@ var stack = require('./lib/stack')
   , config = require('./lib/config');
 
 
+config.app = {
+  loginPage: function(req, res, next) {
+    res.render('login');
+  },
+  authorizePage: function(req, res, next) {
+    res.render('authorize');
+  }
+}
+
 var app = module.exports = stack(config);
